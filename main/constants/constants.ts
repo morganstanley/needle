fc116b046ed973fd72965d9295b2a873922a6424
D Hanson <1564426+Davidhanson90@users.noreply.github.com>
2020-11-13 12:10:41 +0000

    Apply correct namespacing to global state variables.

--- main/constants/constants.ts
+++ main/constants/constants.ts
@@ -1,6 +1,6 @@
-export const DI_ROOT_INJECTOR_KEY = '__DI__ROOT__INJECTOR__';
-export const DI_GLOBAL_STATE_STORE = '__DI__GLOBAL_STATE_STORE';
-export const GLOBAL_INSTANCE_MAP = 'GLOBAL_INSTANCE_MAP';
-export const GLOBAL_REGISTRATION_MAP = 'GLOBAL_REGISTRATION_MAP';
-export const GLOBAL_CONFIGURATION = 'GLOBAL_CONFIGURATION';
+export const DI_ROOT_INJECTOR_KEY = 'morganstanley.com.root.injector';
+export const DI_GLOBAL_STATE_STORE = 'morganstanley.com.state.store';
+export const GLOBAL_INSTANCE_MAP = 'morganstanley.com.instance.map';
+export const GLOBAL_REGISTRATION_MAP = 'morganstanley.com.registrations.map';
+export const GLOBAL_CONFIGURATION = 'morganstanley.com.configuration';
 export const INJECTOR_TYPE_ID = '5495541b-7416-42a6-a830-065fe591591a';
