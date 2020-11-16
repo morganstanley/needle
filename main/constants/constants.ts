059a7695cdf9d06c5bd05023e8f1a394e780d026
D Hanson <1564426+Davidhanson90@users.noreply.github.com>
2020-11-16 08:30:56 +0000

    update namespaces

--- main/constants/constants.ts
+++ main/constants/constants.ts
@@ -1,6 +1,6 @@
-export const DI_ROOT_INJECTOR_KEY = 'morganstanley.com.root.injector';
-export const DI_GLOBAL_STATE_STORE = 'morganstanley.com.state.store';
-export const GLOBAL_INSTANCE_MAP = 'morganstanley.com.instance.map';
-export const GLOBAL_REGISTRATION_MAP = 'morganstanley.com.registrations.map';
-export const GLOBAL_CONFIGURATION = 'morganstanley.com.configuration';
+export const DI_ROOT_INJECTOR_KEY = 'needle.morganstanley.com.root.injector';
+export const DI_GLOBAL_STATE_STORE = 'needle.morganstanley.com.state.store';
+export const GLOBAL_INSTANCE_MAP = 'needle.morganstanley.com.instance.map';
+export const GLOBAL_REGISTRATION_MAP = 'needle.morganstanley.com.registrations.map';
+export const GLOBAL_CONFIGURATION = 'needle.morganstanley.com.configuration';
 export const INJECTOR_TYPE_ID = '5495541b-7416-42a6-a830-065fe591591a';
