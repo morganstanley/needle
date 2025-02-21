cd9b092351622d20bf6d057a3bde6d887d0c1aeb
David Hanson <1564426+Davidhanson90@users.noreply.github.com>
2025-02-21 12:21:24 +0000

    Ensure metadata returned from registration first before falling back to reflect metadata.

--- main/core/metadata.functions.ts
+++ main/core/metadata.functions.ts
@@ -1,3 +1,5 @@
+import { getRootInjector } from './util.functions';
+
 /* Consumers must import the "reflect-metadata" polyfill. */
 declare const Reflect: any;
 
@@ -5,5 +7,12 @@ declare const Reflect: any;
  * Gets an array of types defined in a types constructor
  */
 export function getConstructorTypes<T = unknown>(constr: T): any[] {
-    return Reflect.getMetadata('design:paramtypes', constr) || [];
+    const registration = getRootInjector().getRegistrationForType(constr);
+    if (registration != null && registration.metadata != null) {
+        return registration.metadata;
+    } else if (Reflect != null) {
+        return Reflect.getMetadata('design:paramtypes', constr) || [];
+    }
+
+    return [];
 }
