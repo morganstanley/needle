762da6a89994964fcf49068160324cccf51450c0
David Hanson <1564426+Davidhanson90@users.noreply.github.com>
2025-07-07 09:15:46 +0100

    Simplify cache guards to use typeof checks only

--- main/core/guards.ts
+++ main/core/guards.ts
@@ -103,13 +103,9 @@ export function isDestroyable(value: any): value is IDestroyable {
 }
 
 export function isIdleCacheStrategy(value: any): value is IdleCacheStrategyType {
-    return value != null && (value as IdleCacheStrategyType).timeout != null;
+    return value != null && typeof (value as IdleCacheStrategyType).timeout === 'number';
 }
 
 export function isConditionalCacheStrategy(value: any): value is ConditionalCacheStrategyType {
-    return (
-        value != null &&
-        (value as ConditionalCacheStrategyType).predicate != null &&
-        typeof (value as ConditionalCacheStrategyType).predicate === 'function'
-    );
+    return value != null && typeof (value as ConditionalCacheStrategyType).predicate === 'function';
 }
